52162cf1cb36a76ec78ee4ed1c5e2370e1f01ab1
Andi Alfian Pratama Putra <fianputra15798@gmail.com>
2020-08-08 21:42:46 +0800

    Updates

---
+++ precache-manifest.b6da008ae6d8dacc35501ee2ed80bb5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787c43a69665ac2b7aa0ce051f925880",
+    "url": "/react_internship/index.html"
+  },
+  {
+    "revision": "a548b4dca85eb93b3266",
+    "url": "/react_internship/static/js/2.7ecacf10.chunk.js"
+  },
+  {
+    "revision": "0165b9e48c7d5a0f922345b2e15335c2",
+    "url": "/react_internship/static/js/2.7ecacf10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b925b28c06c43213d2a",
+    "url": "/react_internship/static/js/main.12df39cb.chunk.js"
+  },
+  {
+    "revision": "72ce24b1c79ff3003704",
+    "url": "/react_internship/static/js/runtime-main.298d6f36.js"
+  }
+]);
