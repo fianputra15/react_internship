702d4ceb96075abb1679b1e3c1d0739e49731258
fianputra15 <42870741+fianputra15@users.noreply.github.com>
2020-08-10 09:17:59 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,132 +1,48 @@
 // Material Examples
-import AppBar from './Material/app-bar';
-import Autocomplete from './Material/autocomplete';
-import Avatars from './Material/avatars';
 import BackendError from './Errors/BackendError';
-import Badges from './Material/badges';
 // Pages
 import Blank from './Pages/Blank';
 // Master
 import Magang from './Master/Magang';
 import Users from './Master/Users';
 import Pengaturan from "./Master/Pengaturan";
-import Buttons from './Material/buttons';
 import Laporan from './Master/Laporan';
 import AbsensiMagang from "./Master/Absensi";
 import UbahPassword from "./Master/UbahPassword";
 // Magang
 import Absensi from "./Magang/Absensi";
 import History from "./Magang/History";
-// Apps
-import Calendar from './Apps/Calendar';
-import Cards from './Material/cards';
-// Charts
-import Charts from './Charts/Charts';
-import Chat from './Apps/Chat';
-import Chips from './Material/chips';
-import Detail from './Ecommerce/Detail';
-import Dialogs from './Material/dialogs';
-import Dividers from './Material/dividers';
-import Drawers from './Material/drawers';
-// Form
-import Editor from './Forms/Editor';
-import ExpansionPanels from './Material/expansion-panels';
-// Maps
-import Google from './Maps/Google';
-import GridList from './Material/grid-list';
-// Home
 import Home from './Home/Home';
 import Invoice from './Pages/Invoice';
-import Leaflet from './Maps/Leaflet';
-import Lists from './Material/lists';
-// Authentication
 import Lockscreen from './Authentication/Lockscreen';
-import Media from './Apps/Media';
-import Menus from './Material/menus';
-import Messages from './Apps/Messages';
 // Error
 import NotFound from './Errors/NotFound';
-import Paper from './Material/paper';
 import PasswordReset from './Authentication/PasswordReset';
-import Pickers from './Material/pickers';
 import PricingPage from './Pages/Pricing';
 // Ecommerce
-import Products from './Ecommerce/Products';
-import Progress from './Material/progress';
-import SelectionControls from './Material/selection-controls';
-import Selects from './Material/selects';
 import Signin from './Authentication/Signin';
 import Signup from './Authentication/Signup';
-import Snackbars from './Material/snackbars';
-import Social from './Apps/Social';
-import Steppers from './Material/steppers';
-import Tables from './Material/tables';
-import Tabs from './Material/tabs';
-// Taskboard
-import Taskboard from './Taskboard/Taskboard';
-import TextFields from './Material/text-fields';
 import TimelinePage from './Pages/Timeline';
-import Tooltips from './Material/tooltips';
 // Widgets
-import Widgets from './Widgets/Widgets';
 
 export {
   Home,
-  Calendar,
-  Chat,
-  Media,
   Magang,
   History,
   Absensi,
   Pengaturan,
   Users,
-  Messages,
   AbsensiMagang,
   UbahPassword,
-  Social,
-  Google,
-  Leaflet,
   Blank,
   Invoice,
   PricingPage,
   TimelinePage,
-  Taskboard,
-  Widgets,
-  Charts,
-  Editor,
   Lockscreen,
   PasswordReset,
   Signin,
   Signup,
   NotFound,
   BackendError,
-  Products,
-  Detail,
-  AppBar,
   Laporan,
-  Autocomplete,
-  Avatars,
-  Badges,
-  // ButtonNavigation,
-  Buttons,
-  Cards,
-  Chips,
-  Dialogs,
-  Dividers,
-  Drawers,
-  ExpansionPanels,
-  GridList,
-  Lists,
-  Menus,
-  Paper,
-  Pickers,
-  Progress,
-  SelectionControls,
-  Selects,
-  Snackbars,
-  Steppers,
-  Tables,
-  Tabs,
-  TextFields,
-  Tooltips
 };
